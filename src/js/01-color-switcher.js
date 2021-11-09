629bd634126ecad9ba1750cecc4da28b813efb3e
lanitochka <70783335+lanitochka@users.noreply.github.com>
2021-11-09 22:07:37 +0100

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 
+startBtn.addEventListener('click', onClick);
+stopBtn.addEventListener('click', onStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onClick() {
+
+    timerId = setInterval(() => {
+       document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    startBtn.setAttribute('disabled', true);    
+}
+
+function onStop() {
+
+    clearInterval(timerId);
+    startBtn.removeAttribute('disabled');
+}
